66cd84347841be99cde974e7aac555291e2aba57
DannyAllegrezza <danny@dannyallegrezza.com>
2022-06-06 21:02:47 -0400

    chore: delete unused Component

--- app/components/Chat/ChatDisplay.tsx
+++
@@ -1,9 +0,0 @@
-import type { LoaderFunction } from "@remix-run/node";
-
-export const loader: LoaderFunction = async ({ params }) => {
-	return getWorkspaceById(params.workspace as string);
-};
-
-export default function ChatDisplay() {
-	return <div></div>;
-}
