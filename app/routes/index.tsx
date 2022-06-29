c9606c8d49a4d41e717d58a68d78543391566162
DannyAllegrezza <danny@dannyallegrezza.com>
2022-06-28 23:59:53 -0400

    WIP: mock login functionality

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,87 +1,68 @@
+import { ActionFunction, LoaderFunction, MetaFunction, redirect } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+import { getWorkspaceById, getWorkspacesForUser } from "~/data/workspaces.server";
+
+function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<div className="flex justify-center items-center h-screen bg-[url('/endless-constellation.svg')]">{children}</div>
+	);
+}
+
+function LoginForm() {
+	return (
+		<div className="container px-4 mx-auto">
+			<div className="max-w-md mx-auto py-12 px-8 bg-gray-200 rounded-lg text-center">
+				<span className="text-sm text-gray-500 font-semibold uppercase">Sign Up</span>
+				<h3 className="mb-8 text-3xl font-bold font-heading">Create new account</h3>
+				<Form method="post">
+					<input
+						className="w-full py-3 pl-3 mb-4 bg-white rounded-lg"
+						name="email"
+						type="email"
+						placeholder="E-mail address"
+					/>
+
+					<button className="w-full inline-block px-6 py-3 mb-4 text-sm text-white font-bold leading-loose bg-gray-500 hover:bg-gray-600 rounded transition duration-200">
+						Sign Up
+					</button>
+				</Form>
+				{/* <p className="text-sm text-gray-500">
+					<span>Already have an account?</span>{" "}
+					<a className="font-bold hover:underline" href="#">
+						Sign In
+					</a>
+				</p> */}
+			</div>
+		</div>
+	);
+}
+
+export const meta: MetaFunction = () => {
+	return {
+		title: "Login",
+	};
+};
+
+export const loader: LoaderFunction = async ({}) => {
+	return null;
+};
+
+export const action: ActionFunction = async ({ request }) => {
+	const formData = await request.formData();
+	const email = formData.get("email");
+	console.log(`email: ${email}`);
+
+	// TODO: Implement actual login functionality here :)
+	const workspaces = getWorkspacesForUser();
+	const remixWorkspace = getWorkspaceById(workspaces[0].id);
+
+	return redirect(`/workspace/${remixWorkspace.id}/${remixWorkspace.channels[0].id}`);
+};
+
 export default function Index() {
-  return (
-    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <div>
-          <img
-            className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-            alt="Workflow"
-          />
-          <h2 className="mt-6 text-center text-3xl font-extrabold">Sign in to your account</h2>
-          <p className="mt-2 text-center text-sm">
-            Or{" "}
-            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-              start your 14-day free trial
-            </a>
-          </p>
-        </div>
-        <form className="mt-8 space-y-6" action="#" method="POST">
-          <input type="hidden" name="remember" defaultValue="true" />
-          <div className="rounded-md shadow-sm -space-y-px">
-            <div>
-              <label htmlFor="email-address" className="sr-only">
-                Email address
-              </label>
-              <input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Email address"
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Password"
-              />
-            </div>
-          </div>
-
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                className="h-4 w-4 focus:ring-indigo-500 border-gray-300 rounded"
-              />
-              <label htmlFor="remember-me" className="ml-2 block text-sm">
-                Remember me
-              </label>
-            </div>
-
-            <div className="text-sm">
-              <a href="#" className="font-medium hover:text-indigo-500">
-                Forgot your password?
-              </a>
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                {/* <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" /> */}
-              </span>
-              Sign in
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+	return (
+		<Layout>
+			<LoginForm />
+		</Layout>
+	);
 }
